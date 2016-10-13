a50e55ea4604ce6de57f176a3bb86bf935f5f671
anderson.silva <andersonsrsilva@gmail.com>
2016-10-13 13:40:48 -0300

    Upload files

--- app/Http/Controllers/Contacts.php
+++ app/Http/Controllers/Contacts.php
@@ -8,12 +8,26 @@ use Illuminate\Http\Request;
 
 class Contacts extends Controller
 {
+    private $limit = 5;
+    private $rules = [
+        'name'    => ['required', 'min:5'],
+        'company' => ['required'],
+        'email'   => ['required', 'email'],
+        'photo'   => ['mimes:jpg,jpeg,png,gif,bmp'],
+    ];
+    private $upload_dir = 'public/uploads';
+
+    public function __construct()
+    {
+        $this->upload_dir = base_path() . '/' . $this->upload_dir;
+    }
+
     public function index(Request $request)
     {
-        if($group_id = ($request->get('group_id'))) {
-            $contacts = Contact::where('group_id', $group_id)->orderBy('id', 'desc')->paginate(4);
-        }else {
-            $contacts = Contact::orderBy('id', 'desc')->paginate(4);
+        if ($group_id = ($request->get('group_id'))) {
+            $contacts = Contact::where('group_id', $group_id)->orderBy('id', 'desc')->paginate($this->limit);
+        } else {
+            $contacts = Contact::orderBy('id', 'desc')->paginate($this->limit);
         }
 
         return view('contacts.index', compact('contacts'));
@@ -26,15 +40,11 @@ class Contacts extends Controller
 
     public function store(Request $request)
     {
-        $rules = [
-            'name' => ['required', 'min:5'],
-            'company' => ['required'],
-            'email' => ['required', 'email'],
-        ];
+        $this->validate($request, $this->rules);
 
-        $this->validate($request, $rules);
+        $data = $this->getRequest($request);
 
-        Contact::create($request->all());
+        Contact::create($data);
 
         return redirect('contacts')->with('message', 'Contact Saved!');
     }
@@ -46,16 +56,60 @@ class Contacts extends Controller
 
     public function edit($id)
     {
-        //
+        $contact = Contact::find($id);
+
+        return view('contacts.edit', compact('contact'));
     }
 
-    public function update(Request $request, $id)
+    public function update($id, Request $request)
     {
-        //
+        $this->validate($request, $this->rules);
+
+        $data = $this->getRequest($request);
+        $contact = Contact::find($id);
+
+        $oldPhoto = $contact->photo;
+
+        $contact->update($data);
+
+        if ($oldPhoto !== $contact->photo) {
+            $this->removePhoto($oldPhoto);
+        }
+
+        return redirect('contacts')->with('message', 'Contact Updated!');
     }
 
     public function destroy($id)
     {
-        //
+        $contact = Contact::find($id);
+        $contact->delete();
+
+        $this->removePhoto($contact->photo);
+
+        return redirect('contacts')->with('message', 'Contact Deleted!');
+    }
+
+    private function getRequest($request)
+    {
+        $data = $request->all();
+
+        if ($request->hasFile('photo')) {
+            $photo = $request->file('photo');
+            $fillName = $photo->getClientOriginalName();
+            $destination = $this->upload_dir;
+            $photo->move($destination, $fillName);
+            $data['photo'] = $fillName;
+        }
+
+        return $data;
+    }
+
+    private function removePhoto($photo)
+    {
+        if (!empty($photo)) {
+            $file_path = $this->upload_dir . '/' . $photo;
+
+            if (file_exists($file_path)) unlink($file_path);
+        }
     }
 }
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -9,7 +9,8 @@
                         <div class="media">
                             <div class="media-left">
                                 <a href="#">
-                                    <img class="media-object" src="http://placehold.it/100x100" alt="...">
+                                    <?php $photo = !is_null($contact->photo) ? $contact->photo : 'default.png'; ?>
+                                    {!! Html::image('uploads/' . $photo, $contact->name, ['class' => 'media-object', 'width' => '100', 'height' => 100]) !!}
                                 </a>
                             </div>
                             <div class="media-body">
@@ -23,12 +24,14 @@
                     </td>
                     <td width="100" class="middle">
                         <div>
-                            <a href="#" class="btn btn-circle btn-default btn-xs" title="Edit">
-                                <i class="glyphicon glyphicon-edit"></i>
-                            </a>
-                            <a href="#" class="btn btn-circle btn-danger btn-xs" title="Edit">
-                                <i class="glyphicon glyphicon-remove"></i>
-                            </a>
+                            {!! Form::open(['method' => 'DELETE', 'route' => ['contacts.destroy', $contact->id]]) !!}
+                                <a href="{{ route("contacts.edit", ['id' => $contact->id]) }}" class="btn btn-circle btn-default btn-xs" title="Edit">
+                                    <i class="glyphicon glyphicon-edit"></i>
+                                </a>
+                                <button onclick="return confirm('Are you sure?');" type="submit" class="btn btn-circle btn-danger btn-xs" title="Edit">
+                                    <i class="glyphicon glyphicon-remove"></i>
+                                </button>
+                            {!! Form::close() !!}
                         </div>
                     </td>
                 </tr>
